a3026f588e070ecf11012d30220d8d3e1a8ef1a1
yashi8596 <ec2-user@ip-172-31-12-163.ap-northeast-1.compute.internal>
2022-08-20 11:27:35 +0000

    [Finish]Action,Route,View記述終了(細かい追記等は後日予定)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,12 +18,19 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def update
+    book = Book.find(params[:id])
+    book.update
+    redirect_to '/books/:id'
   end
 
   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
 
   private
