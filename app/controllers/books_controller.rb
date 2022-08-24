a956a233813add92c730c9a8e68118ceacee46ab
yashi8596 <deletio.422shinta@gmail.com>
2022-08-24 13:52:04 +0000

    [Finish]画面表示を見本通りに作成、レイアウトの記述は後日行う

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,27 @@
 class BooksController < ApplicationController
-  def new
+
+
+  #def new
+  #  @book = Book.new
+  #end
+
+  def index
     @book = Book.new
+    @books = Book.all
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to '/top'
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
-  def index
-    @books = Book.all
-  end
+
 
   def show
     @book = Book.find(params[:id])
@@ -22,15 +32,17 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update
-    redirect_to '/books/:id'
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
+    render :show
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to '/books'
+    flash[:notice] = "Book was successfully destroyed."
+    redirect_to books_path(book.id)
   end
 
   private
