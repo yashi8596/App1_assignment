ef9a8eeca1dee40ec9344b36e694207e325fabfa
yashi8596 <ec2-user@ip-172-31-12-163.ap-northeast-1.compute.internal>
2022-08-24 13:52:04 +0000

    [Finish]画面表示を見本通りに作成、レイアウトの記述は後日行う

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'books/new'
-  get '/top' => 'homes#top'
-  post 'books' => 'books#create'
-  get 'books' => 'books#index'
-  get 'books/:id' => 'books#show'
-  get 'books/:id/edit' => 'books#edit'
-  patch 'books/:id' => 'books#update'
-  delete 'books/:id' => 'books#destroy'
+   get '/' => 'homes#top'
+   resources :books
 end
